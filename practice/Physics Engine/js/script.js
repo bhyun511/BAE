b4b0d205cb81ac54f0e7a5fd5717ad8d1801dd06
bhyun511 <54615250+bhyun511@users.noreply.github.com>
2022-03-29 16:32:29 +0900

    Physics Engine

--- practice/Physics Engine/js/script.js
+++ practice/Physics Engine/js/script.js
@@ -1,34 +1,27 @@
+// 1. 박스 우측을 드래그하면
+
 var element = document.querySelector('.right-box');
 var rightBox = new Hammer(element);
-rightBox.get('pen').set({direction: Hammer.DIRECTION_ALL});
+rightBox.get('pan').set({direction: Hammer.DIRECTION_ALL});
 
 var box = $('.box');
 
-rightBox.on('pandown', function(e){
+rightBox.on('pandown', function(e) {
   console.log(e.deltaY);
-
+  // 2. 박스가 우측으로 회전
   box.css('transform-origin', 'left top');
-  box.css('transform', `rotate(${e.deltaY}deg)`);
+  box.css('transform', `rotate(${e.deltaY/10}deg)`);
 
-  var leftValue = perseFloat($('.circle').css('left').slice(0,-2) );
-  if( (leftValue + e.deltaY/2) < 180 ){
-    $('.circle').css('left','+=' + e.deltaY/2);
+  // 3. 동그라미가 우측으로 이동(left: 드래그한 양px)
+  // 4. 근데 이동거리 180px 이하일 때에만 우측으로 이동
+  var leftValue = parseFloat($('.circle').css('left').slice(0, -2)); // 지금 원이 움직인 정도
+  if ((leftValue + e.deltaY/2) < 180) { // 현재 원이 움직인 거리 + 앞으로 이동할 거리가 180 이하일 때에만 실행
+    $('.circle').css('left', '+=' + e.deltaY/2)
   }
 })
 
-rightBox.on('panend', function(e){
+// 5. 박스 원래대로 위치 복구
+rightBox.on('panend', function(e) {
   $('.box').css('transform', 'rotate(0deg)')
+  $('.circle').css('left', 0)
 })
-
-rightBox.click('cicle', function(e){
-  if((rightBox + e.deltaY/3) = 200){
-    $('.circle').css('right','-=' + e.deltaY/5);
-  } else ((leftValue + e.deltaY/3));{
-    $('.circle').css('left','-=' + e.deltaY/5);
-  }
-})
-
-// var value = Refactoring(18);
-
-// new Hammer(element) = rightBox.on();
-// var pandown = $('.pandown');
